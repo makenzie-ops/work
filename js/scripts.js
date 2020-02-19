3285b1e2dd3a67e517b2d7cbc1e19f2afee18058
makenzie-ops <laurenmakenzie0@gmail.com>
2020-02-19 12:32:51 +0300

    add initial commit

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+  $(".java").click(function() {
+    $("#hjs").toggle();
+  });
+});
+
+$(document).ready(function () {
+  $(".rates").click(function() {
+    $("#hops").toggle();
+  });
+});
+$(document).ready(function() {
+  $(".jQ").click(function(){
+    $("#hjq").toggle();
+  });
+});
+$(document).ready(function(){
+  $(".fun").click(function(){
+    $("#hfunc").toggle();
+  });
+});
+
